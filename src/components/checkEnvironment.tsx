8bca4d5a4e7a0bc71bc3411fdd80ba13d0a0e2b2
sarthakkumar766 <sarthakkumar766@gmail.com>
2023-04-28 18:31:31 +0530

    some production deployment changes

--- src/components/checkEnvironment.tsx
+++ src/components/checkEnvironment.tsx
@@ -2,7 +2,7 @@ export const checkEnvironment = () => {
   let base_url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/api/"
-      : "https://example.com/api/"; // https://v2ds.netlify.app
+      : "https://task-survey-application.vercel.app/api/"; // https://v2ds.netlify.app
 
   return base_url;
 };
